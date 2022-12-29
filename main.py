98e27066163161bb97337a80320f28dea0540d10
hmr1973 <99620961+hmr1973@users.noreply.github.com>
2022-12-29 11:16:47 +0000

    sadassa

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
 from flask import Flask, jsonify
 import os
 
-app = Flask(__name__)
+import math
+import feedparser
+from random import seed
+from random import randint
+import textwrap
 
+from PIL import Image 
+from PIL import ImageFont
+from PIL import ImageDraw
+from datetime import datetime
+import urllib.request
+from PIL import Image
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    
+    urllib.request.urlretrieve(
+      'https://source.unsplash.com/1080x1080/',
+       "gfg.png")
+  
+    img = Image.open("gfg.png")
 
+    draw = ImageDraw.Draw(img)
+
+    d = feedparser.parse('https://sucesso.hmr1973.com/feed/')
+
+    return 'ok'
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
