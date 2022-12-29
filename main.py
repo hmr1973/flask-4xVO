bd75ae374e86479944419701d9430e177c7862f9
hmr1973 <99620961+hmr1973@users.noreply.github.com>
2022-12-29 11:48:26 +0000

    sadsadsa

--- main.py
+++ main.py
@@ -20,18 +20,18 @@ app = Flask(__name__)
 def index():
     
     urllib.request.urlretrieve(
-      'https://source.unsplash.com/1080x1080/',
-       "gfg.png")
-  
+    'https://source.unsplash.com/1080x1080/',
+    "gfg.png")
+    
     img = Image.open("gfg.png")
 
     draw = ImageDraw.Draw(img)
 
     d = feedparser.parse('https://sucesso.hmr1973.com/feed/')
-
+    
     w, h = 1080, 300
     shape = [(0, 0), (w, h)]
- 
+    
     # create  rectangleimage
     img1 = ImageDraw.Draw(img)  
     draw.rectangle(shape, fill ="#000000", outline ="black")
@@ -42,24 +42,42 @@ def index():
 
     titulo = d['entries'][value]['title'] 
     corpo = d['entries'][value]['description'] 
-    corpo1 = d['entries'][value]['description']
+    #corpo1 = d['entries'][value]['description']
 
-    corpo1 = corpo1.replace("Quero saber mais sobre como ter sucesso no mundo digital", "")
+    corpo = corpo.replace("Quero saber mais sobre como ter sucesso no mundo digital", "")
 
     corpo = corpo.replace("[&#8230;]", "...")
 
-    text = '\n\n' + titulo + '\n\n' + corpo + '\n\nfonte: https://sucesso.hmr1973.com/ \n\nFollow me @hmr1973maia \n\n#bigdata #weworklabs #entrepreneur #innovation #cycling #datascience #innovation #technology #tech #design #business #engineering #startup #entrepreneur #science #entrepreneurship #future #marketing #creativity #architecture #sustainability #inspiration #ai #art #gadgets #digital #motivation #automation'
+    text = '\n\n' + titulo + '\n\n' + corpo + '\n\nfonte: https://sucesso.hmr1973.com/ \n\n#love #instagood #photooftheday #beautiful #followme #happy #picoftheday #instadaily #fun #instalike #likeforlike #follow #selfie #summer #art #fashion #food #travel #nature #fitness #beauty #workout #friends #family #instamood #photography'
 
     font = ImageFont.truetype("Roboto-Medium.ttf", 65)
-    font1 = ImageFont.truetype("Roboto-Medium.ttf", 40)
+    #font1 = ImageFont.truetype("Roboto-Medium.ttf", 40)
 
     textwrapped = textwrap.wrap(titulo, width=30)
     draw.text((0,10), '\n'.join(textwrapped), font=font, fill="#FFA500")
 
-    textwrapped1 = textwrap.wrap(corpo1, width=50)
+    #textwrapped1 = textwrap.wrap(corpo1, width=50)
     #draw.text((0,700), '\n'.join(textwrapped1), font=font1, fill="#FFA500")
 
     img.save('cur_time.jpg')
+
+    import os
+
+    from instauto.api.client import ApiClient
+    from instauto.api import structs as st
+    from instauto.api.actions import post as ps
+
+
+    if os.path.isfile('./.instauto.save'):
+        client = ApiClient.initiate_from_file('./.instauto.save')
+    else:
+        client = ApiClient(username=os.environ.get("INSTAUTO_USER") or "hmr1973maia", password=os.environ.get("INSTAUTO_PASS") or "Mkonji32!!!?")
+        client.log_in()
+        client.save_to_disk('./.instauto.save')
+
+    post = ps.PostFeed(path='./cur_time.jpg',caption=text)
+    resp = client.post_post(post, 80)
+    print("Success: ", resp.ok)
 
     return text
 
